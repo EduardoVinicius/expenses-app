961ffc7b498eb2e1b38a0c29fff678897c567cc2
Eduardo Vinicius Souza da S Correia <eduardo.correia@detran.pe.gov.br>
2023-03-07 13:09:02 -0300

    Seventh commit

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,81 +1,52 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final void Function(String) onRemove;
 
-  TransactionList(this.transactions, this.onRemove);
+  const TransactionList(this.transactions, this.onRemove);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.7,
-      child: transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx, constraints) {
-              return Column(children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Nenhuma transação cadastrada!',
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: constraints.maxHeight * 0.6,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ))
-              ]);
-            })
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (context, index) {
-                final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text('R\$${tr.value.toStringAsFixed(2)}')),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                    trailing: MediaQuery.of(context).size.width > 400
-                        ? TextButton.icon(
-                            onPressed: () => onRemove(tr.id),
-                            icon: Icon(Icons.delete,
-                                color: Theme.of(context).colorScheme.error),
-                            label: Text(
-                              'Excluir',
-                              style: TextStyle(
-                                  color: Theme.of(context).colorScheme.error,
-                                  fontWeight: FontWeight.bold),
-                            ))
-                        : IconButton(
-                            onPressed: () => onRemove(tr.id),
-                            color: Theme.of(context).colorScheme.error,
-                            icon: const Icon(Icons.delete),
-                          ),
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: transactions.isEmpty
+            ? LayoutBuilder(builder: (ctx, constraints) {
+                return Column(children: [
+                  const SizedBox(
+                    height: 20,
                   ),
-                );
-              },
-            ),
-    );
+                  Text(
+                    'Nenhuma transação cadastrada!',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: constraints.maxHeight * 0.6,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      ))
+                ]);
+              })
+            : ListView(
+                children: transactions.map((tr) {
+                  return TransactionItem(
+                      key: ValueKey(tr.id), tr: tr, onRemove: onRemove);
+                }).toList(),
+              )
+        // ListView.builder(
+        //     itemCount: transactions.length,
+        //     itemBuilder: (context, index) {
+        //       final tr = transactions[index];
+        //       return TransactionItem(tr: tr, onRemove: onRemove);
+        //     },
+        //   ),
+        );
   }
 }
